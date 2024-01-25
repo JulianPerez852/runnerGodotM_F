bfd8a429418d00dc03d1594777d62b403b5eb4af
Julián <jdph852@gmail.com>
2024-01-24 20:54:28 -0500

    added animations to character_robot_1

--- scenas/character_robot_1/robot_1.gd
+++ scenas/character_robot_1/robot_1.gd
@@ -3,42 +3,67 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
+enum state { idle, running, jumping, tochdown }
 
+var current_state = state.idle
+var already_play_animation = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var sprite_animation = $AnimatedSprite2D/AnimationTree
+@onready var sprite_animation = $AnimatedSprite2D/AnimationPlayer
+@onready var ray_cast = $RayCast2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		print("esta en el aire")
 	
-	if is_on_floor():
-		print("toco el suelo")
-		sprite_animation["parameters/conditions/is_jumping"] = false
-		sprite_animation["parameters/conditions/is_tochdown"] = true
+	move(delta)
+	move_and_slide()
+	play_animations()
 
+func move(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		sprite_animation["parameters/conditions/is_idle"] = false
-		sprite_animation["parameters/conditions/is_moving"] = false
-		sprite_animation["parameters/conditions/is_jumping"] = true
+		current_state = state.jumping
+		return
 		
-		print("SALTOOOO")
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	var movement
 	if direction:
 		velocity.x = direction * SPEED
-		sprite_animation["parameters/conditions/is_idle"] = false
-		sprite_animation["parameters/conditions/is_moving"] = true
+		movement = state.running
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		sprite_animation["parameters/conditions/is_idle"] = true
-		sprite_animation["parameters/conditions/is_moving"] = false
+		movement = state.idle
+		
+	fall(delta, movement)
+		
+func fall(delta, movement):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y > -10 and ray_cast.enabled ==false: 
+			ray_cast.enabled = true
+			
+		if ray_cast.is_colliding():
+			current_state = state.tochdown
 
-	move_and_slide()
+	else:
+		current_state = movement
+		ray_cast.enabled = false
+
+func play_animations():
+	if current_state == state.idle:
+		sprite_animation.play("idle")
+	elif current_state == state.running:
+		sprite_animation.play("running")
+	elif current_state == state.jumping:
+		if already_play_animation == false:
+			sprite_animation.play("jumping")
+			already_play_animation = true
+	elif current_state == state.tochdown:
+		if already_play_animation == true:
+			sprite_animation.play("tochdown")
+			already_play_animation = false
+		
